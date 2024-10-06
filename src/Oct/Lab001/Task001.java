71144f055902da111312af1191d152ed6d7a67ea
Divya <divyachoudhari@gmail.com>
2024-10-06 19:47:06 +0300

    commit task1

---
+++ src/Oct/Lab001/Task001.java
@@ -0,0 +1,21 @@
+package Oct.Lab001;
+
+public class Task001
+{
+    public static void main(String[] args)
+    {
+        System.out.println("A programme to learn SOP statement");
+        System.out.println("Table of 10");
+        System.out.println("10*1="+(10*1));
+        System.out.println("10*2="+(10*2));
+        System.out.println("10*3="+(10*3));
+        System.out.println("10*4="+(10*4));
+        System.out.println("10*5="+(10*5));
+        System.out.println("10*6="+(10*6));
+        System.out.println("10*7="+(10*7));
+        System.out.println("10*8="+(10*8));
+        System.out.println("10*9="+(10*9));
+        System.out.println("10*10="+(10*10));
+    }
+
+}
