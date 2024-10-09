8ae23de57453a47999907581843045a11a64fc65
Divya <divyachoudhari@gmail.com>
2024-10-09 15:51:58 +0300

    commit task2

---
+++ src/Oct/Lab002/Task002.java
@@ -0,0 +1,22 @@
+package Oct.Lab002;
+
+public class Task002
+{
+    public static void main(String[] args)
+    {
+        int i = 10;
+
+        System.out.println("A programme to learn SOP statement");
+        System.out.printf("Table of %d \n",i);
+        System.out.printf("%d * %d = %d \n",i,1,i);
+        System.out.printf("%d * %d = %d \n",i,2,i*2);
+        System.out.printf("%d * %d = %d \n",i,3,i*3);
+        System.out.printf("%d * %d = %d \n",i,4,i*4);
+        System.out.printf("%d * %d = %d \n",i,5,i*5);
+        System.out.printf("%d * %d = %d \n",i,6,i*6);
+        System.out.printf("%d * %d = %d \n",i,7,i*7);
+        System.out.printf("%d * %d = %d \n",i,8,i*8);
+        System.out.printf("%d * %d = %d \n",i,9,i*9);
+        System.out.printf("%d * %d = %d \n",i,i,i*i);
+    }
+}
